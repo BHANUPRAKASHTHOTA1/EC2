18ba76ac50744e519151efed649a4b8b8fd1f0dc
Bhanu <k_redu@outlook.com>
2024-01-11 12:24:36 +0530

    added ce2 automation script

--- ec2.sh
+++ ec2.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 NAMES=("mangodb" "cart" "mysql" "shipping" "user" "payment" "rabbitmq" "redis" "catalogue" "web")
+INSTANCE_TYPE=""
+IMAGE_ID=ami-03265a0778a880afb
+SECURITY_GROUP_ID=sg-06ce6a064fac8ab76
+#if mysql and mongodb instance_type should be in t3.medium and for all other t2.micro
 
 for i in "${NAMES[@]}"
 do
-    echo "NAMES : $i"
+    if[ $i == "mongodb"] || $i == "mysql" ]
+    then
+        INSTANCE_TYPE="t3.medium"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+
+    echo "Creating $i Instance"
+    aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
 done
