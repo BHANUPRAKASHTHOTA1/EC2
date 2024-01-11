17299d3610e73406a05ed93546c6c4c970cdf7e7
Bhanu <k_redu@outlook.com>
2024-01-11 12:26:17 +0530

    added ce2 automation script

--- ec2.sh
+++ ec2.sh
@@ -7,7 +7,7 @@ SECURITY_GROUP_ID=sg-06ce6a064fac8ab76
 
 for i in "${NAMES[@]}"
 do
-    if[ $i == "mongodb"] || $i == "mysql" ]
+    if [[ $i == "mongodb"] || $i == "mysql" ]]
     then
         INSTANCE_TYPE="t3.medium"
     else
